fefe227c497b86156b64ceaebe8f1edcb2a59922
Pranav Kumar <pranav.jenus@gmail.com>
2024-10-22 00:28:46 +0530

    Update TUF_No_Of_Jumps_For_Frog.cpp

--- TUF_No_Of_Jumps_For_Frog.cpp
+++ TUF_No_Of_Jumps_For_Frog.cpp
@@ -85,6 +85,52 @@ int fun_With_Memoization(int index,vector<int>ht,vector<int>& dp)
 
 }
 
+int fun_With_Tabulation(vector<int> ht)
+{
+    int n = ht.size();
+    cout << "n=" << n << endl;
+    vector<int> dp(n+1,0);
+    dp[0]=0;
+    for(int i=1;i<n+1;i++)
+    {
+        int fs = dp[i-1]+abs(ht[i]-ht[i-1]);
+        int ss = INT_MAX;
+        if(i>1)
+        {
+            ss = dp[i-2]+abs(ht[i]-ht[i-2]);
+        }
+        dp[i]=(fs>ss)?ss:fs;
+    }
+
+    for(auto d:dp)
+    {
+        cout << d << ",";
+    }
+    return dp[n-1];  // The value to reach n is stored at n-1
+}
+
+int fun_With_SpaceOptimation(vector<int> ht)
+{
+    int n = ht.size();
+    cout << "n=" << n << endl;
+    int prev1=0;
+    int prev2 = 0;
+    int cur;
+    for(int i=1;i<n;i++)
+    {
+        int fs = prev1+abs(ht[i]-ht[i-1]);
+        int ss = INT_MAX;
+        if(i>1)
+        {
+            ss = prev2+abs(ht[i]-ht[i-2]);
+        }
+        cur=(fs>ss)?ss:fs;
+        prev2=prev1;
+        prev1=cur;
+    }
+
+    return prev1;  // The value to reach n is stored at n-1 and is reached by prev1
+}
 
 
 int main()
@@ -98,8 +144,17 @@ int main()
 
     vector<int> dp;
     dp.resize(ht.size()+1,-1);
-    int ans = fun_With_Memoization(ht.size()-1,ht,dp);
+    //int ans = fun_With_Memoization(ht.size()-1,ht,dp);
+    //cout << "MinEneryLost Possible =" << ans << endl;
+
+   // int ans = fun_With_Tabulation(ht);
+    //cout << "MinEneryLost Possible =" << ans << endl;
+
+
+    int ans = fun_With_SpaceOptimation(ht);
     cout << "MinEneryLost Possible =" << ans << endl;
+
+
 
     return 0;
 }
