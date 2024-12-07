ca5cce8290250211cf23ce3b64a3fe43e13ba171
Pranav Kumar <pranav.jenus@gmail.com>
2024-12-07 12:28:18 +0530

    Update TUF_Stocks_Buy_Sell_2.cpp

--- TUF_Stocks_Buy_Sell_2.cpp
+++ TUF_Stocks_Buy_Sell_2.cpp
@@ -35,102 +35,92 @@ Condition : One can have 1 stock in hand,
             One can either buy or sell stock in a day, Can't do both
             Once someone has stock, First it has to be sold,
             then only on the next day stocks can be bought
+
+Note Start from Canbuy as true It means you can start with Empty Container of stocks
+If you start with CanBuy as false, It means you are holding stock already and wants to start from Selling the stock
 */
 
 
-int maxProfitInStocks(int index,bool isBought,vector<int> SPrices)
+int maxProfitInStocks(int index,bool canBuy,vector<int> SPrices)
 {
     if(index==SPrices.size())
     {
         return 0;
     }
 
-    // isBought = true means stock is held and can only be sold now
-    // isBought = false means no stock is held and can only be Buy Stock now
+    // canBuy = true stock can be bought and held
+    // canBuy = false stock can be sold now
 
-    if(isBought)
+    if(canBuy)
     {
-        return max ( SPrices[index]+maxProfitInStocks(index+1,false,SPrices) ,
+        return max( -SPrices[index]+maxProfitInStocks(index+1,false,SPrices),
                      0 + maxProfitInStocks(index+1,true,SPrices) );
     }
     else
     {
-        return max( -SPrices[index]+maxProfitInStocks(index+1,true,SPrices),
+        return max ( SPrices[index]+maxProfitInStocks(index+1,true,SPrices) ,
                      0 + maxProfitInStocks(index+1,false,SPrices) );
     }
 }
 
-int maxProfitInStocks(int index,int isBought,vector<int> SPrices,vector<vector<int>>& dp)
+int maxProfitInStocks(int index,int canBuy,vector<int> SPrices,vector<vector<int>>& dp)
 {
     if(index==SPrices.size())
     {
         return 0;
     }
 
-    if(dp[index][isBought]!=-1)
+    if(dp[index][canBuy]!=-1)
     {
         cout << "Returned from dp" << endl;
-        return dp[index][isBought];
+        return dp[index][canBuy];
     }
 
-    // isBought = true means stock is held and can only be sold now
-    // isBought = false means no stock is held and can only be Buy Stock now
 
     int profit = 0;
-    if(isBought)
+    if(canBuy)
     {
-        profit = max ( SPrices[index]+maxProfitInStocks(index+1,0,SPrices,dp) ,
-                        0 + maxProfitInStocks(index+1,isBought,SPrices,dp) );
-
+        profit = max( -SPrices[index]+maxProfitInStocks(index+1,false,SPrices,dp), // Can sell since bought
+                     0 + maxProfitInStocks(index+1,true,SPrices,dp) ); // Can buy Only
     }
     else
     {
-        profit = max( -SPrices[index]+maxProfitInStocks(index+1,1,SPrices,dp),
-                        0 + maxProfitInStocks(index+1,isBought,SPrices,dp) );
-        cout << "index =" << index << " Profit=" << profit << endl;
+        profit = max ( SPrices[index]+maxProfitInStocks(index+1,true,SPrices,dp) , // Can Buy since sold
+                     0 + maxProfitInStocks(index+1,false,SPrices,dp) ); // Can sell Only
     }
 
-    return dp[index][isBought] = profit;
+    return dp[index][canBuy] = profit;
 }
 
 int maxProfitInStocks(vector<int> SPrices)
 {
     int m = SPrices.size();
-    vector<vector<int>> dp(m+1,vector<int>(2,-1));
+    vector<vector<int>> dp(m+1,vector<int>(2,0));
 
     for(int j=0;j<2;j++)
         dp[m][j]=0;
 
 
-    // isBought = true means stock is held and can only be sold now
-    // isBought = false means no stock is held and can only be Buy Stock now
-
     for(int index=m-1;index>=0;index--)
     {
-        for(int isBought=0;isBought<2;isBought++)
+        for(int canBuy=0;canBuy<2;canBuy++)
         {
             int profit = 0;
-            if(isBought) // isBought = 1
+            if(canBuy)
             {
-                profit = max ( SPrices[index]+dp[index+1][0] ,
-                                0 + dp[index+1][isBought] );
-                cout << "index =" << index << " Profit=" << profit << endl;
-                dp[index][isBought] = profit;
-                //break;
+                profit = max( -SPrices[index]+dp[index+1][0], // Can sell since bought
+                             0 + dp[index+1][1] ); // Can buy Only
             }
             else
             {
-                profit = max( -SPrices[index]+dp[index+1][1] ,
-                                0 + dp[index+1][isBought] );
-                cout << "index =" << index << " Profit=" << profit << endl;
-                dp[index][isBought] = profit;
-                //break;
+               profit = max ( SPrices[index]+dp[index+1][1], // Can Buy since sold
+                             0 + dp[index+1][1] ); // Can sell Only
             }
-
+            dp[index][canBuy] = profit;
         }
     }
     print_2D_int_array(dp);
-    return dp[0][0];
+    return dp[0][1];
 }
 
 int maxProfitInStocks_Space(vector<int> SPrices)
@@ -143,47 +133,39 @@ int maxProfitInStocks_Space(vector<int> SPrices)
         Next[j]=0;
 
 
-    // isBought = true means stock is held and can only be sold now
-    // isBought = false means no stock is held and can only be Buy Stock now
-
     for(int index=m-1;index>=0;index--)
     {
-        for(int isBought=0;isBought<2;isBought++)
+        for(int canBuy=0;canBuy<2;canBuy++)
         {
             int profit = 0;
-            if(isBought) // isBought = 1
+            if(canBuy)
             {
-                profit = max ( SPrices[index]+Next[0] ,
-                                0 + Next[isBought] );
-                cout << "index =" << index << " Profit=" << profit << endl;
-                Cur[isBought] = profit;
-                //break;
+                profit = max( -SPrices[index]+Next[0], // Can sell since bought
+                             0 + Next[1] ); // Can buy Only
             }
             else
             {
-                profit = max( -SPrices[index]+Next[1] ,
-                                0 + Next[isBought] );
-                cout << "index =" << index << " Profit=" << profit << endl;
-                Cur[isBought] = profit;
-                //break;
+               profit = max ( SPrices[index]+Next[1] , // Can Buy since sold
+                             0 + Next[1] ); // Can sell Only
             }
+            Cur[canBuy] = profit;
 
         }
         Next = Cur;
     }
     print_1D_int_array(Next);
-    return Next[0];
+    return Next[1];
 }
 
 int main()
 {
     vector<int> SPrices = {7,1,5,3,6,4};
-    int ans = maxProfitInStocks(0,false,SPrices);
+    int ans = maxProfitInStocks(0,true,SPrices); // Start from Can Buy , ** Do not start from true -> Means start from Sell
     cout << "Max - Profit -- InStocks =" << ans << endl;
 
     int m = SPrices.size();
     vector<vector<int>> dp(m,vector<int>(2,-1));
-    ans = maxProfitInStocks(0,0,SPrices,dp);
+    ans = maxProfitInStocks(0,1,SPrices,dp);
     print_2D_int_array(dp);
     cout << "Max - Profit -- InStocks Using Memoization=" << ans << endl;
 
